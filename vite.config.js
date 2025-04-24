0e83c7169fb8b6bee1134fd64abdb287dab1f1fd
Abdalrhman213 <kizruju1@gmail.com>
2025-04-24 16:28:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// 👇 غيّر المسار بناءً على اسم الريبو على GitHub
+// 👇 اسم الريبو على GitHub
 const repoName = 'cryptocraft-ai';
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, // 👈 هذا مهم جداً لـ GitHub Pages
+  base: `/${repoName}/`, // 👈 مهم جداً للنشر على GitHub Pages
 });
